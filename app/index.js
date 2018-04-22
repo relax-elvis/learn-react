0936880a57ee09aae6f6a7f3ea0a857038ce05f3
elvis <m.v.veklyk@gmail.com>
2018-04-23 00:03:57 +0300

    learn require function and use

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 let words = require('./output');
-let word = words.Breakdown;
+let word = words;
 let string = "Well, it's one for the money, Two for the show, Three to get ready";
 let result = word(string);
 console.log(result);
